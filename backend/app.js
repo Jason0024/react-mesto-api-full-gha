47742d45cf61b9e726c78715ad37bbc09257a535
Sergey <s.shamrdin@gmail.com>
2023-06-12 16:59:34 +0000

    eslint

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env; 
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const cors = require('cors');
 // Защита сервера
 const rateLimit = require('express-rate-limit');
@@ -16,25 +16,24 @@ const mainRouter = require('./routes/index');
 
 const app = express();
 app.use(cors());
-//CORS config
+// CORS config
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE" // what matters here is that OPTIONS is present
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE', // what matters here is that OPTIONS is present
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
-//CORS alowd sites
-//const allowedCors = [
+// CORS alowd sites
+// const allowedCors = [
 //  'https://jason.student.nomoredomains.rocks/',
 //  'http://jason.student.nomoredomains.rocks/',
 //  'http://localhost:3000',
 //  'http://localhost:3001',
-//];
-
+// ];
 
 // Для защиты от множества автоматических запросов
 // https://www.npmjs.com/package/express-rate-limit
